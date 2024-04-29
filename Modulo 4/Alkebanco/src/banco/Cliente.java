6ee360822d64c3ba1dd56153378e7601476ee641
jioh19 <jioh1982@gmail.com>
2024-04-29 10:02:13 -0400

    Init alkebanco

---
+++ Modulo 4/Alkebanco/src/banco/Cliente.java
@@ -0,0 +1,40 @@
+package banco;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Cliente {
+    private String nombre;
+    private String apellido;
+    private String rut;
+    private String password;
+    List<Cuenta> cuentas;
+
+    public Cliente(String nombre, String apellido, String rut, String password) {
+        super();
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.rut = rut;
+        this.password = password;
+        cuentas = new LinkedList<Cuenta>();
+
+    }
+
+    public void addCuenta(Cuenta c) {
+        cuentas.add(c);
+    }
+
+    @Override
+    public String toString() {
+        //*****************************************************
+        return String.format("**%15s%-10s%10s%-14s**\n", "Nombre:", nombre, "Apellido:", apellido)+
+                String.format("**%15s%-10s%10s%-14s**\n", "Password:", password, "RUT:", rut)+
+                "*****************************************************\n";
+
+//		return "Cliente [nombre=" + nombre + ", apellido=" + apellido + ", rut=" + rut + ", password=" + password
+//				+ ", cuentas=" + cuentas + "]";
+    }
+
+
+
+}
