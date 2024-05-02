13d534eb18c01bccd4da9ccad8f989f54e91a895
jioh19 <jioh1982@gmail.com>
2024-05-02 13:18:31 -0400

    Ayudantia 2024-05-02

---
+++ Modulo 4/Ayudantia 2024-05-02/src/menu/Main.java
@@ -0,0 +1,14 @@
+package menu;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.dibujaMenu();
+//        System.out.println("Ingresa una seleccion");
+//        Scanner leer = new Scanner(System.in);
+//        int opcion = leer.nextInt();
+//        System.out.println("Has escaneado el numero " + opcion);
+    }
+}
---
+++ Modulo 4/Ayudantia 2024-05-02/src/menu/Menu.java
@@ -0,0 +1,18 @@
+package menu;
+
+public class Menu {
+
+    public void dibujaMenu() {
+//        System.out.println(" 1- Sumar");
+//        System.out.println(" 2- Restar");
+        String menu = """
+                ************************************************
+                ** 1- Sumar                                   **
+                ** 2- Restar                                  **
+                ** 3- Multiplicar                             **
+                ** 4- Dividir                                 **
+                ** 5- Salir                                   **
+                ************************************************""";
+        System.out.println(menu);
+    }
+}
