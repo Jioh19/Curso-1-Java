fdba557431a16daa3d4206e6a963633080a5b6c7
jioh19 <jioh1982@gmail.com>
2024-05-02 21:18:23 -0400

    Ayudantia 2024-05-02

---
+++ Modulo 4/Ayudantia 2024-05-02/src/Calculadora/Calculadora.java
@@ -0,0 +1,20 @@
+package Calculadora;
+
+public class Calculadora {
+
+    public double sumar(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public double restar(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public double dividir(double num1, double num2) {
+        return num1 / num2;
+    }
+
+    public double multiplicar(double num1, double num2) {
+        return num1 * num2;
+    }
+}
--- Modulo 4/Ayudantia 2024-05-02/src/menu/Main.java
+++ Modulo 4/Ayudantia 2024-05-02/src/menu/Main.java
@@ -1,14 +1,71 @@
 package menu;
 
 import java.util.Scanner;
+import Calculadora.Calculadora;
+
+import javax.swing.*;
+
+import static javax.swing.JOptionPane.INFORMATION_MESSAGE;
 
 public class Main {
     public static void main(String[] args) {
         Menu menu = new Menu();
-        menu.dibujaMenu();
-//        System.out.println("Ingresa una seleccion");
-//        Scanner leer = new Scanner(System.in);
-//        int opcion = leer.nextInt();
-//        System.out.println("Has escaneado el numero " + opcion);
+        Calculadora calcu = new Calculadora();
+        boolean continuar = true;
+
+//        Object[] values = {"Sumar", "Restar"};
+//
+//        Object value = JOptionPane.showInputDialog(null, "Seleccion operacion", "Input",
+//                JOptionPane.INFORMATION_MESSAGE, null,
+//                values, values[0]);
+
+        while(continuar) {
+
+            menu.dibujaMenu();
+            System.out.println("** Ingresa una seleccion                      **");
+            Scanner leer = new Scanner(System.in);
+            int opcion = leer.nextInt();
+
+            if(opcion == 1) {
+                System.out.println("Has seleccionado la suma");
+                System.out.println("Ingresa el primero número");
+                double num1 = leer.nextDouble();
+                System.out.println("Ingresa el segundo número");
+                double num2 = leer.nextDouble();
+                double resultado = calcu.sumar(num1, num2);
+                System.out.println("La suma es " + resultado);
+            } else if(opcion == 2) {
+                System.out.println("Has seleccionado la resta");
+                System.out.println("Ingresa el primero número");
+                double num1 = leer.nextDouble();
+                System.out.println("Ingresa el segundo número");
+                double num2 = leer.nextDouble();
+                double resultado = calcu.restar(num1, num2);
+                System.out.println("La resta es " + resultado);
+            } else if(opcion == 3) {
+                System.out.println("Has seleccionado la multiplicación");
+                System.out.println("Ingresa el primero número");
+                double num1 = leer.nextDouble();
+                System.out.println("Ingresa el segundo número");
+                double num2 = leer.nextDouble();
+                double resultado = calcu.multiplicar(num1, num2);
+                System.out.println("La multiplicación es " + resultado);
+            } else if(opcion == 4) {
+                System.out.println("Has seleccionado la división");
+                System.out.println("Ingresa el primero número");
+                double num1 = leer.nextDouble();
+                System.out.println("Ingresa el segundo número");
+                double num2 = leer.nextDouble();
+                double resultado = calcu.dividir(num1, num2);
+                System.out.println("La division es " + resultado);
+            } else if(opcion == 5) {
+                System.out.println("Has seleccionado salir");
+                continuar = false;
+            } else {
+                System.out.println("Has ingresado un valor inválido");
+            }
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+        }
     }
 }
